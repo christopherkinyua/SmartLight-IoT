9ff39244dfc14fb7933da226c093973467cb24a7
christopherkinyua <christopher.kinyua@ucalgary.ca>
2024-10-28 22:39:50 -0600

    Added check for valid hexadecimal representation in function arg helpers

--- firmware/Core/Inc/rgb.h
+++ firmware/Core/Inc/rgb.h
@@ -14,6 +14,9 @@ typedef struct {
     uint8_t RGB_blue;  // Blue component (0-255)
 } LED_RGB_t;
 
+
+uint8_t RGB_convert_hex_to_rgb(char *hex_color_code, LED_RGB_t *rgb_result);
+
 #ifdef __cplusplus
 }
 #endif
