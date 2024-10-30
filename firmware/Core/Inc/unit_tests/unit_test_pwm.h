8877af6e0bee6491ae56a75ff765b72cffe4bd5a
christopherkinyua <christopher.kinyua@ucalgary.ca>
2024-10-30 03:45:38 -0600

    Created unit test system that runs on the esp32c3

---
+++ firmware/Core/Inc/unit_tests/unit_test_pwm.h
@@ -0,0 +1,8 @@
+#ifndef __INCLUDE_GUARD__UNIT_TEST_PWM_H__
+#define __INCLUDE_GUARD__UNIT_TEST_PWM_H__
+
+#include <stdint.h>
+
+uint8_t TEST_EXEC__LED_set_dimming();
+
+#endif // __INCLUDE_GUARD__UNIT_TEST_PWM_H__
--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -1,23 +1,39 @@
+#include "main.h"
+#include "IO.h"
+#include "pwm.h"
+#include "unit_tests/unit_test_executor.h"
+
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_log.h"
 
-#include "main.h"
-#include "IO.h"
-#include "pwm.h"
-
-
-void app_main(void) {
+void app_main(void)
+{
 
     GPIO_init();
     LED_PWM_Init();
-    ESP_LOGI("Main", "Hello, ESP32-C3!");
+
+    char response_output_buf[2048] = {0};
+    uint8_t unit_tests_result = TEST_run_all_unit_tests_and_log(response_output_buf, sizeof(response_output_buf));
+    printf(response_output_buf);
+
+    if (unit_tests_result)
+    {
+        printf("\nall tests passed\n");
+    }
+    else
+    {
+        printf("\nsome tests failed\n");
+    }
+
+        ESP_LOGI("Main", "Hello, ESP32-C3!");
 
     // For demonstration, create a simple task
-    while (true) {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);  // Delay 1 second
+    while (true)
+    {
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay 1 second
         ESP_LOGI("Main", "Running...");
         LED_test_dimming();
     }
